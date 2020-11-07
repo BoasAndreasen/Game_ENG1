900cddde605a3c0b2b65614a23b4228e7cd6559a
ElizabethHowells <65850099+ElizabethHowells@users.noreply.github.com>
2020-11-07 14:47:36 +0000

    Adding basic constructor for system

--- Lizards/core/src/com/mygdx/game/model/System.java
+++ Lizards/core/src/com/mygdx/game/model/System.java
@@ -3,4 +3,13 @@ package com.mygdx.game.model;
 import com.badlogic.gdx.math.Rectangle;
 
 public class System extends Rectangle {
+
+    private int health;
+    private boolean isDestroyed; 
+
+    public System(float x,float y, float height,float width, int health,boolean isDestroyed){
+        super(x, y, width, height);
+        this.health=health;
+        this.isDestroyed=isDestroyed;
+    }
 }
