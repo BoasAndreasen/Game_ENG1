14a9bde5f600b2a400659038c4bc93a899b79227
ElizabethHowells <65850099+ElizabethHowells@users.noreply.github.com>
2020-11-07 14:49:54 +0000

    adding system to world

--- Lizards/core/src/com/mygdx/game/model/System.java
+++ Lizards/core/src/com/mygdx/game/model/System.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.math.Rectangle;
 public class System extends Rectangle {
 
     private int health;
-    private boolean isDestroyed; 
+    private boolean isDestroyed;
 
     public System(float x,float y, float height,float width, int health,boolean isDestroyed){
         super(x, y, width, height);
--- Lizards/core/src/com/mygdx/game/model/World.java
+++ Lizards/core/src/com/mygdx/game/model/World.java
@@ -6,6 +6,7 @@ public class World {
     private Array<Block> blocks = new Array<Block>();
     private Array<Block> hostiles = new Array<Block>();
     private Auber auber;
+    private System sys1;
 
     public World() {
         createWorld();
@@ -14,9 +15,12 @@ public class World {
     private void createWorld() {
         auber = new Auber(0, 0, 64, 64,1, "left");
         blocks.add(new Block(0, 1, 64, 64));
+        sys1= new System(300,300,64,64,100,false);
     }
 
     public Auber getAuber() {
         return auber;
     }
+
+    public System getSys1(){return sys1;}
 }
