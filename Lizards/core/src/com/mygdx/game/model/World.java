2abd3d7d5bb6c1c5518e4d08a1e8b1922cf8afd1
ElizabethHowells <65850099+ElizabethHowells@users.noreply.github.com>
2020-11-08 10:18:48 +0000

    Created all 15 systems in world
    Left their x and y values to 0 as we don't know where we want them yet.

--- Lizards/core/src/com/mygdx/game/model/World.java
+++ Lizards/core/src/com/mygdx/game/model/World.java
@@ -7,6 +7,21 @@ public class World {
     private Array<Block> hostiles = new Array<Block>();
     private Auber auber;
     private System sys1;
+    private System sys2;
+    private System sys3;
+    private System sys4;
+    private System sys5;
+    private System sys6;
+    private System sys7;
+    private System sys8;
+    private System sys9;
+    private System sys10;
+    private System sys11;
+    private System sys12;
+    private System sys13;
+    private System sys14;
+    private System sys15;
+
 
     public World() {
         createWorld();
@@ -15,12 +30,43 @@ public class World {
     private void createWorld() {
         auber = new Auber(0, 0, 64, 64,1, "left");
         blocks.add(new Block(0, 1, 64, 64));
+
+        //systems created below all with x=0,y=0 for now
         sys1= new System(300,300,64,64,100,false);
+        sys2= new System(0,0,64,64,100,false);
+        sys3= new System(0,0,64,64,100,false);
+        sys4= new System(0,0,64,64,100,false);
+        sys5= new System(0,0,64,64,100,false);
+        sys6= new System(0,0,64,64,100,false);
+        sys7= new System(0,0,64,64,100,false);
+        sys8= new System(0,0,64,64,100,false);
+        sys9= new System(0,0,64,64,100,false);
+        sys10= new System(0,0,64,64,100,false);
+        sys11= new System(0,0,64,64,100,false);
+        sys12= new System(0,0,64,64,100,false);
+        sys13= new System(0,0,64,64,100,false);
+        sys14= new System(0,0,64,64,100,false);
+        sys15= new System(0,0,64,64,100,false);
+
     }
 
-    public Auber getAuber() {
-        return auber;
-    }
+    public Auber getAuber() {return auber;}
 
     public System getSys1(){return sys1;}
+    public System getSys2(){return sys2;}
+    public System getSys3(){return sys3;}
+    public System getSys4(){return sys4;}
+    public System getSys5(){return sys5;}
+    public System getSys6(){return sys6;}
+    public System getSys7(){return sys7;}
+    public System getSys8(){return sys8;}
+    public System getSys9(){return sys9;}
+    public System getSys10(){return sys10;}
+    public System getSys11(){return sys11;}
+    public System getSys12(){return sys12;}
+    public System getSys13(){return sys13;}
+    public System getSys14(){return sys14;}
+    public System getSys15(){return sys15;}
+
+
 }
