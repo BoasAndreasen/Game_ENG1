7fad134ce1e156a43e5e1f9a83ad01b43f554f18
Tasha <theblankrogue@gmail.com>
2020-11-13 13:43:45 +0000

    Walls rendered onto map
    I've rendered the walls onto all 4 screens of the map now. However I'm getting an error saying list index can't be >= 15...

--- Lizards/core/src/com/mygdx/game/model/World.java
+++ Lizards/core/src/com/mygdx/game/model/World.java
@@ -1,6 +1,7 @@
 package com.mygdx.game.model;
 
 import com.badlogic.gdx.utils.Array;
+import java.util.*;
 import com.mygdx.game.controller.AuberController;
 
 public class World {
@@ -67,9 +68,61 @@ public class World {
 
         auber = new Auber(0, 0, "left");
 
-        horizWall.add(new HorizWall(500, 300));
-        horizWall.add(new HorizWall(700, 300));
-        vertiWall.add(new VertiWall(800, 300));
+        //screen 1 walls
+        List<Integer> screen1HorizWallsX = Arrays.asList(200, 300, 400, 500, 600, 700, 800,
+                900, 1000, 1100, 200, 300, 400, 500, 600, 200, 500, 600, 700, 1000, 1100);
+        List<Integer> screen1HorizWallsY = Arrays.asList(600, 600, 600, 600, 600, 600, 600,
+                600, 600, 600, 800, 800, 800, 800, 800, 1000, 1000, 1000, 1000, 1000, 1000);
+        List<Integer> screen1VertiWallsX = Arrays.asList(200, 200, 700, 700, 700, 700, 1190,
+                1190, 1190, 1190);
+        List<Integer> screen1VertiWallsY = Arrays.asList(800, 900, 600, 700, 800, 900, 600,
+                700, 800, 900);
+        for (int i=0; i < screen1HorizWallsX.size(); i++) {
+            horizWall.add(new HorizWall(screen1HorizWallsX.get(i), screen1HorizWallsY.get(i))); }
+        for (int i=0; i < screen1VertiWallsX.size(); i++) {
+            vertiWall.add(new VertiWall(screen1VertiWallsX.get(i), screen1VertiWallsY.get(i))); }
+
+        //screen 2 walls
+        List<Integer> screen2HorizWallsX = Arrays.asList(1200, 1500, 1800, 1900, 2000, 2300, 1800,
+                2100, 2200, 2300, 1200, 1300, 1400, 1500, 1800, 1900, 2000, 2100, 2200, 2300);
+        List<Integer> screen2HorizWallsY = Arrays.asList(1000, 1000, 1000, 1000, 1000, 1000, 800,
+                800, 800, 800, 600, 600, 600, 600, 600, 600, 600, 600, 600, 600);
+        List<Integer> screen2VertiWallsX = Arrays.asList(1200, 1200, 1200, 1200, 1600, 1600, 1600,
+                1600, 1800, 1800, 1800, 1800);
+        List<Integer> screen2VertiWallsY = Arrays.asList(600, 700, 800, 900, 600, 700, 800, 900,
+                600, 700, 1000, 1100);
+        for (int i=0; i < screen2HorizWallsX.size(); i++) {
+            horizWall.add(new HorizWall(screen2HorizWallsX.get(i), screen2HorizWallsY.get(i))); }
+        for (int i=0; i < screen2VertiWallsX.size(); i++) {
+            vertiWall.add(new VertiWall(screen2VertiWallsX.get(i), screen2VertiWallsY.get(i))); }
+
+        //screen 3 walls
+        List<Integer> screen3HorizWallsX = Arrays.asList(200, 300, 400, 500, 600, 1100, 200,
+                300, 400, 500, 600, 700, 800, 900, 1000, 1100);
+        List<Integer> screen3HorizWallsY = Arrays.asList(200, 200, 200, 200, 200, 200, 590,
+                590, 590, 590, 590, 590, 590, 590, 590, 590);
+        List<Integer> screen3VertiWallsX = Arrays.asList(200, 200, 700, 700, 700, 700, 900,
+                900, 900, 900, 1190, 1190, 1190, 1190);
+        List<Integer> screen3VertiWallsY = Arrays.asList(200, 300, 200, 300, 400, 500, 200,
+                300, 400, 500, 200, 300, 400, 500);
+        for (int i=0; i < screen3HorizWallsX.size(); i++) {
+            horizWall.add(new HorizWall(screen3HorizWallsX.get(i), screen3HorizWallsY.get(i))); }
+        for (int i=0; i < screen3VertiWallsX.size(); i++) {
+            vertiWall.add(new VertiWall(screen3VertiWallsX.get(i), screen3VertiWallsY.get(i))); }
+
+        //screen 4 walls
+        List<Integer> screen4HorizWallsX = Arrays.asList(1200, 1300, 1400, 1500, 1800, 1900,
+                2000, 2100, 2200, 2300, 1200, 1300, 1400, 1500, 1800, 2100, 2200, 2300);
+        List<Integer> screen4HorizWallsY = Arrays.asList(590, 590, 590, 590, 590, 590, 590,
+                590, 590, 590, 200, 200, 200, 200, 200, 200, 200, 200);
+        List<Integer> screen4VertiWallsX = Arrays.asList(1200, 1200, 1200, 1200, 1600, 1600,
+                1800, 1800, 1800, 1800);
+        List<Integer> screen4VertiWallsY = Arrays.asList(200, 300, 400, 500, 200, 500, 200,
+                300, 400, 500);
+        for (int i=0; i < screen4HorizWallsX.size(); i++) {
+            horizWall.add(new HorizWall(screen4HorizWallsX.get(i), screen4HorizWallsY.get(i))); }
+        for (int i=0; i < screen4VertiWallsX.size(); i++) {
+            vertiWall.add(new VertiWall(screen4VertiWallsX.get(i), screen4VertiWallsY.get(i))); }
 
         infiltrator = new Infiltrator(100, 300, "left");
     }
