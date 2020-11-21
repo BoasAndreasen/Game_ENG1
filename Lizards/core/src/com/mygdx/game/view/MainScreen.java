5b56823bd6c3ec35a02d4d61a5c320a532c8934b
Frazzle52 <72562478+Frazzle52@users.noreply.github.com>
2020-11-21 16:54:37 +0000

    Main Menu Guide Added

--- Lizards/core/src/com/mygdx/game/view/MainScreen.java
+++ Lizards/core/src/com/mygdx/game/view/MainScreen.java
@@ -32,30 +32,127 @@ public class MainScreen implements Screen {
         this.game = game;
     }
 	
-    private Label.LabelStyle notify_style;
+    //Text style
+    private Label.LabelStyle menu_style;
     private BitmapFont my_font;
-    private Label notify_label;
+    private Label menu_label;
+    private Label move_label;
+    private Label infiltrator_label;
+    private Label teleport_label;
+    private Label heal_label;
+    private Label system_label;
+    private Label shield_label;
+    private Label bomb_label;
+    private Label corrupt_label;
     private Stage stage;
     private Skin skin;
     
+    //Images
+    private Texture bucketImage; //Auber
+    private Texture infiltratorImage; //Infiltrator
+    private Texture systemImage; //System
+    private Texture teleportPadImage; // TeleportPad
+    private Texture bombImage; //bomb
+    private Texture shieldImage; //shield
+    private Texture corruptImage; //corrupt
+    private Texture healPadImage; // Healing Pads
     
+    private SpriteBatch batch;
 
 
 	@Override
 	public void show() {
 		
-        notify_style = new Label.LabelStyle();
+		//Text style		
+        menu_style = new Label.LabelStyle();
         my_font = new BitmapFont(Gdx.files.internal("my_font.fnt"));
         stage = new Stage();
         skin = new Skin(Gdx.files.internal("clean-crispy-ui.json"));
-        notify_style.font = my_font;
-        notify_style.background = skin.getDrawable("button");
-        notify_label = new Label("Press Any Key to Start",notify_style);
-        notify_label.setSize(900,100);
-        notify_label.setAlignment(Align.center);
-        notify_label.setPosition(Gdx.graphics.getWidth()/2-450,0);
+        menu_style.font = my_font;
+        menu_style.background = skin.getDrawable("button");
         
-        stage.addActor(notify_label);
+        //Images
+        bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+        systemImage = new Texture(Gdx.files.internal("System.png"));
+        infiltratorImage = new Texture(Gdx.files.internal("Infiltrator.png"));
+        teleportPadImage = new Texture(Gdx.files.internal("TeleportPad.png"));
+        bombImage= new Texture(Gdx.files.internal("bomb2.jpg"));
+        shieldImage= new Texture(Gdx.files.internal("shield.png"));
+        corruptImage= new Texture(Gdx.files.internal("corrupt.jpg"));
+        healPadImage = new Texture(Gdx.files.internal("HealPad.png"));
+        
+        //Movement text
+        
+        move_label = new Label("Use WASD or Arrow\nKeys to Move Auber",menu_style);
+        move_label.setSize(450,100);
+        move_label.setAlignment(Align.center);
+        move_label.setPosition(Gdx.graphics.getWidth()/2 - 460, Gdx.graphics.getHeight()-110);
+        stage.addActor(move_label);
+        
+        //Infiltrator text
+        
+        infiltrator_label = new Label("Click Infiltrators\nNearby to Arrest Them",menu_style);
+        infiltrator_label.setSize(450,100);
+        infiltrator_label.setAlignment(Align.center);
+        infiltrator_label.setPosition(Gdx.graphics.getWidth()/2 - 460, Gdx.graphics.getHeight()-220);
+        stage.addActor(infiltrator_label);
+        
+        //Teleporter text
+        
+        teleport_label = new Label("Use Teleporter Pads\nto Get About Quickly",menu_style);
+        teleport_label.setSize(450,100);
+        teleport_label.setAlignment(Align.center);
+        teleport_label.setPosition(Gdx.graphics.getWidth()/2 - 460, Gdx.graphics.getHeight()-330);
+        stage.addActor(teleport_label);
+        
+        //Heal pad text
+        
+        heal_label = new Label("Use the Infirmary's\nHeal Pad if Injured",menu_style);
+        heal_label.setSize(450,100);
+        heal_label.setAlignment(Align.center);
+        heal_label.setPosition(Gdx.graphics.getWidth()/2 - 460, Gdx.graphics.getHeight()-440);
+        stage.addActor(heal_label);
+        
+        //Systems text
+        
+        system_label = new Label("Protect the Ship's\nSystems",menu_style);
+        system_label.setSize(450,100);
+        system_label.setAlignment(Align.center);
+        system_label.setPosition(Gdx.graphics.getWidth()- 460, Gdx.graphics.getHeight()-110);
+        stage.addActor(system_label);
+        
+        //Shield text
+        
+        shield_label = new Label("Shields Block Auber\nFrom Arresting When Up",menu_style);
+        shield_label.setSize(450,100);
+        shield_label.setAlignment(Align.center);
+        shield_label.setPosition(Gdx.graphics.getWidth()- 460, Gdx.graphics.getHeight()-220);
+        stage.addActor(shield_label);
+        
+        //Bomb text
+        
+        bomb_label = new Label("Bombs Deal Greater\nDamage - Watch Out!",menu_style);
+        bomb_label.setSize(450,100);
+        bomb_label.setAlignment(Align.center);
+        bomb_label.setPosition(Gdx.graphics.getWidth()- 460, Gdx.graphics.getHeight()-330);
+        stage.addActor(bomb_label);
+        
+        //Corrupt text
+        
+        corrupt_label = new Label("Corrupt Makes Auber\nDamage Nearby Systems",menu_style);
+        corrupt_label.setSize(450,100);
+        corrupt_label.setAlignment(Align.center);
+        corrupt_label.setPosition(Gdx.graphics.getWidth()- 460, Gdx.graphics.getHeight()-440);
+        stage.addActor(corrupt_label);
+        
+        
+        //Press to start text
+        menu_label = new Label("Press Any Key to Start",menu_style);
+        menu_label.setSize(900,100);
+        menu_label.setAlignment(Align.center);
+        menu_label.setPosition(Gdx.graphics.getWidth()/2-450,0);
+        
+        stage.addActor(menu_label);
         
         Gdx.input.setInputProcessor(new InputAdapter() {
         	
@@ -66,6 +163,8 @@ public class MainScreen implements Screen {
         	      return false;
         	   }			
         });
+        
+        batch = new SpriteBatch();
 	}
 
 	@Override
@@ -74,6 +173,28 @@ public class MainScreen implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);	
         
         stage.draw();
+        
+        batch.begin();
+        
+        //Example image rendering
+        
+        batch.draw(bucketImage, (Gdx.graphics.getWidth()/2 - 460 - bucketImage.getWidth())/2, Gdx.graphics.getHeight()-60-(bucketImage.getHeight()/2));
+        
+        batch.draw(infiltratorImage, (Gdx.graphics.getWidth()/2 - 460 - infiltratorImage.getWidth())/2, Gdx.graphics.getHeight()-170-(infiltratorImage.getHeight()/2));
+
+        batch.draw(teleportPadImage, (Gdx.graphics.getWidth()/2 - 460 - teleportPadImage.getWidth())/2, Gdx.graphics.getHeight()-280-(teleportPadImage.getHeight()/2));
+        
+        batch.draw(healPadImage, (Gdx.graphics.getWidth()/2 - 460 - healPadImage.getWidth())/2, Gdx.graphics.getHeight()-390-(healPadImage.getHeight()/2));
+        
+        batch.draw(systemImage, (Gdx.graphics.getWidth()/2+(Gdx.graphics.getWidth()/2 - 460 - systemImage.getWidth())/2), Gdx.graphics.getHeight()-60-(systemImage.getHeight()/2));
+
+        batch.draw(shieldImage, (Gdx.graphics.getWidth()/2+(Gdx.graphics.getWidth()/2 - 460 - shieldImage.getWidth())/2), Gdx.graphics.getHeight()-170-(shieldImage.getHeight()/2));
+
+        batch.draw(bombImage, (Gdx.graphics.getWidth()/2+(Gdx.graphics.getWidth()/2 - 460 - bombImage.getWidth())/2), Gdx.graphics.getHeight()-280-(bombImage.getHeight()/2));
+
+        batch.draw(corruptImage, (Gdx.graphics.getWidth()/2+(Gdx.graphics.getWidth()/2 - 460 - corruptImage.getWidth())/2), Gdx.graphics.getHeight()-390-(corruptImage.getHeight()/2));
+
+        batch.end();
 	}
 
 	@Override
